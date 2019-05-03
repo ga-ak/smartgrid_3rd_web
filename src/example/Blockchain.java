b8ec46d17fd6ff100bf7fb9193073dcb198131b4
cheolho <gghg0000@gmail.com>
2019-05-03 16:56:56 +0900

    19.05.03 16:56 kch

--- src/example/Blockchain.java
+++
@@ -1,7 +0,0 @@
-package example;
-
-public class Blockchain {
-    public static void main(String[] args) {
-        System.out.println("블록체인 만들기");
-    }
-}
--- src/example/Hyemin.java
+++
@@ -1,8 +0,0 @@
-package example;
-
-public class Hyemin {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-
-}
